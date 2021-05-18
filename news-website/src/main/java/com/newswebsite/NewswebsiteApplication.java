c606fd696e8adb66f40ebc5e505ea575c7c4d47a
Hakan <46871570+hakan2259@users.noreply.github.com>
2021-05-19 00:47:08 +0100

    Update project

--- news-website/src/main/java/com/newswebsite/NewswebsiteApplication.java
+++ news-website/src/main/java/com/newswebsite/NewswebsiteApplication.java
@@ -15,32 +15,15 @@ import com.newswebsite.service.UserService;
 import com.newswebsite.utility.SecurityUtility;
 
 @SpringBootApplication
-public class NewswebsiteApplication implements CommandLineRunner  {
+public class NewswebsiteApplication   {
 
-	@Autowired
-	private UserService userService;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(NewswebsiteApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		
-		User user1 = new User();
-		user1.setFirstname("John");
-		user1.setLastname("Adams");
-		user1.setUsername("j59");
-		user1.setPassword(SecurityUtility.passwordEncoder().encode("59"));
-		user1.setEmail("jAdams@gmail.com");
-		user1.setRoleId(2);
-		Set<UserRole> userRoles = new HashSet<>();
-		Role role1 = new Role();
-		role1.setRoleId(2);
-		role1.setName("ROLE_USER");
-		userRoles.add(new UserRole(user1,role1));
-		
-		userService.createUser(user1,userRoles);
+	
 		
 		
 		
@@ -48,4 +31,4 @@ public class NewswebsiteApplication implements CommandLineRunner  {
 		
 	}
 
-}
+
